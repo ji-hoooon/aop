dcbb42368f26ca12f584a6f3108a47b68b00fc16
ji-hoooon <ljh6384@naver.com>
2023-04-17 08:38:16 +0900

    ADD INIT COMMIT

---
+++ src/main/java/shop/mtcoding/metamall/model/orderproduct/OrderProductRepository.java
@@ -0,0 +1,6 @@
+package shop.mtcoding.metamall.model.orderproduct;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+}
---
+++ src/main/java/shop/mtcoding/metamall/model/ordersheet/OrderSheetRepository.java
@@ -0,0 +1,6 @@
+package shop.mtcoding.metamall.model.ordersheet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderSheetRepository extends JpaRepository<OrderSheet, Long> {
+}
