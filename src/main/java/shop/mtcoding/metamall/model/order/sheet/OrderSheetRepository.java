153152d0e8a5589dcc5242c39828906a741948c7
ji-hoooon <ljh6384@naver.com>
2023-04-17 10:29:14 +0900

    Repository 완료

--- src/main/java/shop/mtcoding/metamall/model/order/sheet/OrderSheetRepository.java
+++ src/main/java/shop/mtcoding/metamall/model/order/sheet/OrderSheetRepository.java
@@ -1,6 +1,14 @@
 package shop.mtcoding.metamall.model.order.sheet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderSheetRepository extends JpaRepository<OrderSheet, Long> {
+
+    //주문한 목록 보기에 필요한 메서드
+    @Query ("select os from OrderSheet os where os.user.id=:userId")
+    List<OrderSheet> findByUserId(@Param("userId") Long userId);
 }
