0a859dbcc89a20b721e5216ecd6df558ee53480a
ji-hoooon <ljh6384@naver.com>
2023-04-17 10:34:51 +0900

    UPDATE DummyData 세팅 완료

--- src/main/java/shop/mtcoding/metamall/MetamallApplication.java
+++ src/main/java/shop/mtcoding/metamall/MetamallApplication.java
@@ -21,8 +21,8 @@ public class MetamallApplication {
 			// 여기에서 save 하면 됨.
 			// bulk Collector는 saveAll 하면 됨.
 			User ssar = User.builder().username("ssar").password("1234").email("ssar@nate.com").role("USER").status(true).build();
-			User seller = User.builder().username("ssar").password("1234").email("ssar@nate.com").role("SELLER").status(true).build();
-			User admin = User.builder().username("ssar").password("1234").email("ssar@nate.com").role("ADMIN").status(true).build();
+			User seller = User.builder().username("seller").password("1234").email("ssar@nate.com").role("SELLER").status(true).build();
+			User admin = User.builder().username("admin").password("1234").email("ssar@nate.com").role("ADMIN").status(true).build();
 
 			//벌크 컬렉터로 세이브: JPA에서 여러 개의 엔티티를 한번에 조회하는 기능
 			// 이 기능은 단일 쿼리로 대량의 데이터를 한 번에 가져올 수 있기 때문에, 여러 번의 쿼리 실행과 그에 따른 DB 부하를 줄일 수 있다.
